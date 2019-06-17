2c4c29b946beea65ea06d02af9bf3135f0894139
gardnern <48269958+gardnern@users.noreply.github.com>
2019-06-16 18:07:02 -0600

    added friends

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+    {
+        name: "Morgan Freeman",
+        photo: "https://www.bu.edu/today/files/2013/05/v2_MF-Headshot-030813.jpg",
+        scores: [5, 4, 3, 2, 1, 3, 4, 5, 1, 2]
+    },
+    {
+        name: "Leonardo DiCaprio",
+        photo: "https://m.media-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY317_CR10,0,214,317_AL_.jpg",
+        scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 1]
+    },
+    {
+        name: "Johnny Depp",
+        photo: "https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2015/06/johnny_depp_dior.jpg",
+        scores: [2, 3, 4, 5, 5, 4, 3, 2, 1, 2]
+    },
+    {
+        name: "Kirsten Bell",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/18/08/kirstenbell-0.jpg",
+        scores: [5, 4, 3, 2, 1, 1, 3, 4, 4, 5]
+    },
+    {
+        name: "Fan Bingbing ",
+        photo: "https://pmcdeadline2.files.wordpress.com/2016/11/fan-bingbing-e1528101903936.jpg?w=681&h=383&crop=1",
+        scores: [3, 2, 1, 3, 4, 3, 2, 1, 5, 5]
+    },
+    {
+        name: "Naomie Harris",
+        photo: "https://i.pinimg.com/originals/dc/d1/fd/dcd1fd4309b4f69eb8dbef635108b5e5.jpg",
+        scores: [3, 3, 2, 4, 4, 5, 1, 3, 2, 4]
+    }
+
+];
+
+module.exports = friends;
